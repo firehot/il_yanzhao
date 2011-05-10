dc874f10a9c200ecdaa191087323d2b8add333d9
chengdh <cheng.donghui@gmail.com>
2011-05-10 11:12:27 +0800

    add package column to carrying_bill

---
+++ db/migrate/20110509024544_add_package_to_carrying_bill.rb
@@ -0,0 +1,9 @@
+class AddPackageToCarryingBill < ActiveRecord::Migration
+  def self.up
+    add_column :carrying_bills, :package, :string,:limit => 30
+  end
+
+  def self.down
+    remove_column :carrying_bills, :package
+  end
+end
