5162286068d3ec392d82c51ef899d341f5839f50
chengdh <cheng.donghui@gmail.com>
2011-07-19 19:07:26 +0800

    fixed ie6 js bug

--- public/javascripts/ie6.js
+++ public/javascripts/ie6.js
@@ -1,19 +1,9 @@
 /*IE6下的特定处理*/
 jQuery(function($) {
-	$('.table tbody tr').live('hover', function() {
+	$('.table tbody').hover(function() {
 		$(this).css({
-			backgroundColor: '#FFCCCC',
 			cursor: 'pointer'
 		});
-	},
-	function() {
-		$(this).css({
-			backgroundColor: '#EAFDFF'
-		});
-		if ($(this).hasClass('odd')) $(this).css({
-			backgroundColor: '#FFFFFF'
-		});
 	});
-
 });
 
