bddea3c07c1743b1fe0c71c88c6932883f1d79b3
chengdh <cheng.donghui@gmail.com>
2011-09-09 14:34:46 +0800

    fixed short_fee_info bug

--- app/models/carrying_bill.rb
+++ app/models/carrying_bill.rb
@@ -201,14 +201,14 @@ class CarryingBill < ActiveRecord::Base
     #短途运费状态声明
     state_machine :from_short_fee_state,:initial => :draft,:namespace => :from_short_fee do
       #发货地短途运费核销
-      event :from_write_off  do
+      event :write_off  do
         transition :draft => :offed
       end
     end
     #短途运费状态声明
     state_machine :to_short_fee_state,:initial => :draft,:namespace => :to_short_fee do
       #发货地短途运费核销
-      event :to_write_off  do
+      event :write_off  do
         transition :draft => :offed
       end
     end
--- app/models/short_fee_info.rb
+++ app/models/short_fee_info.rb
@@ -12,8 +12,8 @@ class ShortFeeInfo < ActiveRecord::Base
   state_machine :initial => :draft do
     after_transition do |info,transition|
       info.short_fee_info_lines.each do |line|
-        line.carrying_bill.from_write_off if info.org_id.eql?(line.carrying_bill.from_org_id)
-        line.carrying_bill.to_write_off if info.org_id.eql?(line.carrying_bill.to_org_id)
+        line.carrying_bill.write_off_from_short_fee! if info.org_id.eql?(line.carrying_bill.from_org_id)
+        line.carrying_bill.write_off_to_short_fee if info.org_id.eql?(line.carrying_bill.to_org_id)
       end
     end
     event :write_off do
