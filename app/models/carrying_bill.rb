4fbb51704b2b66f3e0bfe84e609a03e712655c5a
chengdh <cheng.donghui@gmail.com>
2012-04-14 10:33:26 +0800

    修改了扣手续费的计算方式，当修改运单的货款时，也重新计算扣手续费

--- app/models/carrying_bill.rb
+++ app/models/carrying_bill.rb
@@ -44,7 +44,8 @@ class CarryingBill < ActiveRecord::Base
   #保存成功后,设置原始费用
   before_create :set_original_fee
   #计算手续费
-  before_create :cal_hand_fee
+  #NOTE 当修改单据的代收货款时，也重新计算运费
+  before_save :cal_hand_fee
 
   belongs_to :user
   belongs_to :from_org,:class_name => "Org"
@@ -511,7 +512,7 @@ class CarryingBill < ActiveRecord::Base
           :transit_org_name,:to_org_name,:pay_type_des,:from_short_carrying_fee,:to_short_carrying_fee,
           :carrying_fee,:carrying_fee_th,:k_carrying_fee,:k_hand_fee,:goods_fee,:insured_fee,:transit_carrying_fee,
           :transit_hand_fee,:act_pay_fee,:agent_carrying_fee,:th_amount,:goods_num,:note,:human_state_name
-      ]}
+        ]}
     end
     #机打运单编号从4000000开始
     #生成票据编号
@@ -536,10 +537,12 @@ class CarryingBill < ActiveRecord::Base
     end
     #计算手续费
     def cal_hand_fee
-      if self.goods_fee_cash?
-        self.k_hand_fee = ConfigCash.cal_hand_fee(:goods_fee => self.goods_fee,:from_org_id => self.from_org_id,:to_org_id => self.to_org_id)
-      else
-        self.k_hand_fee = ConfigTransit.cal_hand_fee(:goods_fee => self.goods_fee,:from_org_id => self.from_org_id,:to_org_id => self.to_org_id)
+      if self.new_record?  or self.changes[:goods_fee].present?
+        if self.goods_fee_cash?
+          self.k_hand_fee = ConfigCash.cal_hand_fee(:goods_fee => self.goods_fee,:from_org_id => self.from_org_id,:to_org_id => self.to_org_id)
+        else
+          self.k_hand_fee = ConfigTransit.cal_hand_fee(:goods_fee => self.goods_fee,:from_org_id => self.from_org_id,:to_org_id => self.to_org_id)
+        end
       end
       self.k_hand_fee
     end
