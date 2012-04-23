74dd577a8d3a962136eda64584fca7e7b965b3a2
chengdh <cheng.donghui@gmail.com>
2012-04-23 16:39:39 +0800

    基本完成了货物分类运费设置功能

--- app/models/goods_cat_fee_config.rb
+++ app/models/goods_cat_fee_config.rb
@@ -4,7 +4,17 @@ class GoodsCatFeeConfig < ActiveRecord::Base
   belongs_to :from_org,:class_name => "Org"
   belongs_to :to_org,:class_name => "Org"
   has_many :goods_cat_fee_config_lines,:dependent => :destroy
-  has_many :goods_cats,:through => :goos_cat_fee_config_lines
+  has_many :goods_cats,:through => :goods_cat_fee_config_lines
   validates :from_org,:to_org,:presence => true
   validates :to_org_id,:uniqueness => {:scope => :from_org_id}
+  accepts_nested_attributes_for :goods_cat_fee_config_lines
+  #构造运费设置明细
+  def all_group_lines
+    #NOTE 此处只支持2级分类
+    GoodsCat.search(:is_active_eq => true,:parent_id_is_not_null => true).order("order_by ASC").each do |child_cat|
+        self.goods_cat_fee_config_lines.build(:goods_cat => child_cat) unless self.goods_cats.include?(child_cat)
+    end
+    @group_lines ||= self.goods_cat_fee_config_lines.sort_by {|line| line.goods_cat.order_by }.group_by {|line| line.goods_cat.parent}
+    @group_lines
+  end
 end
--- db/migrate/20120423074249_add_goods_cat_fee_config_function.rb
+++ db/migrate/20120423074249_add_goods_cat_fee_config_function.rb
@@ -1,14 +1,14 @@
 #coding: utf-8
-class AddGoodsCatFunction < ActiveRecord::Migration
-  def self.up
+class AddGoodsCatFeeConfigFunction < ActiveRecord::Migration
+  def up
     group_name = "系统管理"
     #################################分理处/分公司管理################################################
-    subject_title = "货物分类维护"
-    subject = "GoodsCat"
+    subject_title = "货物分类运费设置"
+    subject = "GoodsCatFeeConfig"
     sf_hash = {
       :group_name => group_name,
       :subject_title => subject_title,
-      :default_action => 'goods_cats_path',
+      :default_action => 'goods_cat_fee_configs_path',
       :subject => subject,
       :function => {
         :read =>{:title => "查看"} ,
@@ -20,8 +20,8 @@ class AddGoodsCatFunction < ActiveRecord::Migration
     SystemFunction.create_by_hash(sf_hash)
   end
 
-  def self.down
-    sf = SystemFunction.find_by_subject_title('货物分类维护')
+  def down
+    sf = SystemFunction.find_by_subject_title('货物分类运费设置')
     sf.destroy if sf
   end
 end
