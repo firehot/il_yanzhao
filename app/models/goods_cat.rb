7bf3c1d1348f007df917e2dce3d9f175e3aa0467
chengdh <cheng.donghui@gmail.com>
2012-05-02 09:33:03 +0800

    修改了carrying_bills_controller查询请求的错误

--- app/models/goods_cat.rb
+++ app/models/goods_cat.rb
@@ -1,4 +1,5 @@
 #coding: utf-8
+require 'ruby-pinyin/pinyin'
 class GoodsCat < ActiveRecord::Base
   acts_as_tree :order => :order_by
   default_scope includes(:goods_cat_promotions)
@@ -7,8 +8,14 @@ class GoodsCat < ActiveRecord::Base
   validates_associated :goods_cat_promotions
   accepts_nested_attributes_for :goods_cat_promotions
   after_initialize :build_goods_cat_promotions
+  before_create :gen_py
   private
   def build_goods_cat_promotions
     (10 - self.goods_cat_promotions.size).times { self.goods_cat_promotions.build }
   end
+  def gen_py
+    py = PinYin.instance
+    self.py = py.to_pinyin_abbr(self.name)
+  end
+
 end
