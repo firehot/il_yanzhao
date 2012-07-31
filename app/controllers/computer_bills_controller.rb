bfec74f9716ff8efbf022a6f2efb2a4977bb4c26
chengdh <cheng.donghui@gmail.com>
2012-07-31 12:05:54 +0800

    remove authorize_resource for debug

--- app/controllers/computer_bills_controller.rb
+++ app/controllers/computer_bills_controller.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
-require 'new_relic/agent/method_tracer'
 class ComputerBillsController < CarryingBillsController
-  include NewRelic::Agent::MethodTracer
   defaults :resource_class => ComputerBill
   include BillOperate::BillPrint
 
-  add_method_tracer :new, 'Custom/computer_bills_new'
 end
