18d3af8ff7d9349e865eb91a0fe0046ba0116530
chengdh <cheng.donghui@gmail.com>
2012-07-31 11:40:17 +0800

    add newrelic metric code for computer bills controller

--- app/controllers/computer_bills_controller.rb
+++ app/controllers/computer_bills_controller.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
+require 'new_relic/agent/method_tracer'
 class ComputerBillsController < CarryingBillsController
   defaults :resource_class => ComputerBill
   include BillOperate::BillPrint
+
+  add_method_tracer :new, 'Custom/computer_bills_new'
 end
