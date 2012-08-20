09dddc394770538736375ed3e5ce004787eb3365
chengdh <cheng.donghui@gmail.com>
2012-08-20 18:37:38 +0800

    修改了理赔功能重复提交的错误

--- app/assets/javascripts/il_application.js
+++ app/assets/javascripts/il_application.js
@@ -159,7 +159,7 @@ jQuery(function($) {
 			return newchar;
 		},
 
-		//导出数据到excel, ie only
+		//导出数据到excel, for none ie browser
 		export_excel: function() {
 			var uri = 'data:application/vnd.ms-excel;base64,',
 			template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
