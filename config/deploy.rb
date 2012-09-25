61f9860f15b280af380e460f5d7020922eb80090
chengdh <cheng.donghui@gmail.com>
2012-09-25 22:13:21 +0800

    添加了capistrano mulitstage的支持

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,55 +1,8 @@
 # -*- encoding : utf-8 -*-
-#add bundler support
-require 'bundler/capistrano'
-set :application, "il_yanzhao_rails32"
-set :repository,  "."
-set :branch, "upgrade_to_rails32"
-set :deploy_via, :copy
-set :copy_cache, true
-#
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :stages, %w(production px_production local_server)
+set :default_stage, "px_production"
+require 'capistrano/ext/multistage'
 
-#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-server "zz.yanzhaowuliu.com",:app,:web,:db,:primary => true
-#server "192.168.0.202",:app,:web,:db,:primary => true
-
-set :user,"lmis"
-set :use_sudo,false
-default_run_options[:pty]=true
-
-set :deploy_to,"~/app/il_yanzhao_rails32"
-
-#set rvm support
-set :rvm_ruby_string, '1.9.3@rails32_gemset'
-#若rvm以user wide 安装,则rvm相关信息设置如下
-#set :rvm_path, "~/.rvm"
-#set :rvm_bin_path, "~/.rvm/bin"
-
-#若rvm以system wide安装,则rvm设置如下
-set :rvm_path, "/usr/local/rvm"
-set :rvm_bin_path, "/usr/local/rvm/bin"
-
-require "rvm/capistrano"
-
-#set unicorn support
-require 'capistrano-unicorn'
-set :unicorn_bin,'r193_unicorn_rails'
-=begin
-set :default_environment, {
-  'PATH' => "/usr/local/rvm/gems/ree-1.8.7-2011.03/bin:/usr/local/rvm/bin:$PATH",
-  'RUBY_VERSION' => 'ree 1.8.7',
-  'GEM_HOME'     => '/usr/local/rvm/gems/ree-1.8.7-2011.01@rails3_gemset',
-  'GEM_PATH'     => '/usr/local/rvm/gems/ree-1.8.7-2011.03:/usr/local/rvm/gems/ree-1.8.7-2011.01@rails3_gemset',
-  'BUNDLE_PATH'     => '/usr/local/rvm/gems/ree-1.8.7-2011.03'
-}
-=end
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
 namespace :rvm do
   task :trust_rvmrc do
     run "rvm rvmrc trust #{release_path}"
---
+++ config/deploy/px_production.rb
@@ -0,0 +1,42 @@
+# -*- encoding : utf-8 -*-
+#add bundler support
+require 'bundler/capistrano'
+set :application, "px_il_yanzhao_rails32"
+set :repository,  "."
+set :branch, "upgrade_to_rails32"
+set :deploy_via, :copy
+set :copy_cache, true
+#
+set :scm, :git
+server "px.yanzhaowuliu.com",:app,:web,:db,:primary => true
+
+set :user,"lmis"
+set :use_sudo,false
+default_run_options[:pty]=true
+
+set :deploy_to,"~/app/px_il_yanzhao_rails32"
+
+#set rvm support
+set :rvm_ruby_string, '1.9.3@rails32_gemset'
+#若rvm以user wide 安装,则rvm相关信息设置如下
+#set :rvm_path, "~/.rvm"
+#set :rvm_bin_path, "~/.rvm/bin"
+
+#若rvm以system wide安装,则rvm设置如下
+set :rvm_path, "/usr/local/rvm"
+set :rvm_bin_path, "/usr/local/rvm/bin"
+
+require "rvm/capistrano"
+
+#set unicorn support
+require 'capistrano-unicorn'
+set :unicorn_bin,'r193_unicorn_rails'
+
+after "deploy:symlink", :update_database_yml
+desc "根据不同的staging修改数据库名称"
+task :update_database_yml do
+  replacements = { "il_yanzhao_r32_production" => "il_yanzhao_r32_production" }
+  replacements.each_pair do |pattern, sub|
+    run "sed -i 's/#{pattern}/#{sub}/' ~/app/il_yanzhao_rails32/current/config/database.yml"
+  end
+end
