3aeb343a9370cf5e6c539859a0f46793038d1a0b
chengdh <cheng.donghui@gmail.com>
2012-09-25 23:05:26 +0800

    修改了update_database_yml部分的代码

--- config/deploy/local_server.rb
+++ config/deploy/local_server.rb
@@ -8,7 +8,7 @@ set :deploy_via, :copy
 set :copy_cache, true
 #
 set :scm, :git
-server "localhost",:app,:web,:db,:primary => true
+server "127.0.0.1",:app,:web,:db,:primary => true
 
 set :user,"chengdh"
 set :use_sudo,false
@@ -32,10 +32,12 @@ require "rvm/capistrano"
 require 'capistrano-unicorn'
 set :unicorn_bin,'r193_unicorn_rails'
 
-after "deploy:symlink", :update_database_yml
 desc "根据不同的staging修改数据库名称"
 task :update_database_yml do
-  replacements = { "il_yanzhao_r32_production" => "il_yanzhao_r32_production" }
+  replacements = {
+    "il_yanzhao_r32_production" => "il_yanzhao_r32_production",
+    "\/var\/run\/mysqld\/mysqld.sock" => "\/tmp\/mysql.sock" #mac 系统下mysql.sock位置与linux不同
+  }
   replacements.each_pair do |pattern, sub|
     run "sed -i 's/#{pattern}/#{sub}/' ~/app/il_yanzhao_rails32/current/config/database.yml"
   end
