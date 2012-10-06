b7eaebf99f2140b036b8eb737e823183008a8f1d
chengdh <cheng.donghui@gmail.com>
2012-10-06 11:04:04 +0800

    修改了运单导出

--- app/models/carrying_bill_extend/scope.rb
+++ app/models/carrying_bill_extend/scope.rb
@@ -6,12 +6,12 @@ module CarryingBillExtend
       base.extend(ClassMethods)
       base.class_eval do
         #营业额统计
-        scope :turnover,select('from_org_id,IFNULL(to_org_id,transit_org_id) as to_org_id,sum(carrying_fee) as sum_carrying_fee,sum(goods_fee) as sum_goods_fee,sum(goods_num) as sum_goods_num,sum(1) as sum_bill_count').where(["state <> ? ","invalided"]).group('from_org_id,to_org_id')
+        scope :turnover,select('from_org_id,IFNULL(to_org_id,transit_org_id) as to_org_id,sum(carrying_fee) as sum_carrying_fee,sum(goods_fee) as sum_goods_fee,sum(insured_fee) as sum_insured_fee,sum(from_short_carrying_fee) as sum_from_short_carrying_fee,sum(to_short_carrying_fee) as to_short_carrying_fee,sum(goods_num) as sum_goods_num,sum(1) as sum_bill_count').where(["state <> ? ","invalided"]).group('from_org_id,to_org_id')
 
         #今日收货
-        scope :today_billed,lambda {|from_org_ids| select('pay_type,sum(carrying_fee) as carrying_fee,sum(goods_fee) as goods_fee,sum(goods_num) as goods_num,sum(1) as bill_count').where(:from_org_id => from_org_ids,:bill_date => Date.today).where(["state <> ? ","invalided"]).group(:pay_type)}
+        scope :today_billed,lambda {|from_org_ids| select('pay_type,sum(carrying_fee) as carrying_fee,sum(goods_fee) as goods_fee,sum(insured_fee) as insured_fee,sum(from_short_carrying_fee) as from_short_carrying_fee,sum(to_short_carrying_fee) as to_short_carrying_fee,sum(goods_num) as goods_num,sum(1) as bill_count').where(:from_org_id => from_org_ids,:bill_date => Date.today).where(["state <> ? ","invalided"]).group(:pay_type)}
         #今日提货
-        scope :today_deliveried,lambda {|to_org_ids| select('type,pay_type,sum(carrying_fee) as carrying_fee,sum(goods_fee)  as goods_fee,sum(goods_num) as goods_num,sum(1) as bill_count').where(:to_org_id => to_org_ids,:state =>:deliveried).search(:deliver_info_deliver_date_eq => Date.today).group('type,pay_type')}
+        scope :today_deliveried,lambda {|to_org_ids| select('type,pay_type,sum(carrying_fee) as carrying_fee,sum(goods_fee)  as goods_fee,sum(insured_fee) as insured_fee,sum(from_short_carrying_fee) as from_short_carrying_fee,sum(to_short_carrying_fee) as to_short_carrying_fee,sum(goods_num) as goods_num,sum(1) as bill_count').where(:to_org_id => to_org_ids,:state =>:deliveried).search(:deliver_info_deliver_date_eq => Date.today).group('type,pay_type')}
         #今日提款(仅指现金提款)
         scope :today_paid,lambda {|from_org_ids| select('pay_type,sum(carrying_fee) as carrying_fee,sum(insured_fee) as insured_fee,sum(from_short_carrying_fee) as from_short_carrying_fee,sum(to_short_carrying_fee) as to_short_carrying_fee,sum(goods_fee) as goods_fee,sum(goods_num) as goods_num,sum(k_hand_fee) as k_hand_fee,sum(transit_hand_fee) as transit_hand_fee,sum(1) as bill_count').where(:from_org_id => from_org_ids,:state =>:paid).search(:from_customer_id_is_null => 1,:pay_info_bill_date_eq => Date.today).group('pay_type')}
         #最近票据(5张)
