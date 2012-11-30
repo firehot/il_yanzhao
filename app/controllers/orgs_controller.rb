8bf25fda4cc371161da1c2752980f074d7699513
chengdh <cheng.donghui@gmail.com>
2012-11-30 21:03:29 +0800

    修改了orgs_controller的代码

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -3,6 +3,9 @@ class OrgsController < BaseController
   #使用http缓存数据到客户端
   http_cache :index,:last_modified => Proc.new {|c| c.send(:last_modified)},:etag => Proc.new {|c| c.send(:etag,"org_index")}
   skip_before_filter :authenticate_user!,:only => [:index]
+  protect_from_forgery :except => [:index]
+  skip_load_and_authorize_resource
+
   #提取所有机构
   def index
     @orgs = Org.search(params[:search]).order('parent_id ASC,created_at DESC')
